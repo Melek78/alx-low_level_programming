8d3f9c012ae056d610bb16ce6248854637ea2276
Melek78 <abemelek78@gmail.com>
2022-09-08 15:14:28 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  Description: main -prints a string to stdout .
+ *  Return: 0 if success.
+ */
+int main(void)
+{
+	puts("\"programming is like buliding a multilingual puzzle"
+	    );
+		return (0);
+}
