cf554ab2e3fe318c95f29ef183c68e98730f0f71
Melek78 <abemelek78@gmail.com>
2022-09-09 20:20:10 +0300

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,9 +7,9 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
-char n;
-for (n = 'a'; n <= 'z'; n++) putchar(n);
+{ char n;
+for (n = 'a'; n <= 'z'; n++)
+putchar(n);
 putchar('\n');
 return (0);
 }
