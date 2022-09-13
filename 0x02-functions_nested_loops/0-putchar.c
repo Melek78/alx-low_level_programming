a00eb924b09bb61ccaaecb7a6dfb2fd03e87f75d
Melek78 <abemelek78@gmail.com>
2022-09-13 20:28:49 +0300

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+
+/**
+* main - Print the word "_putchar"
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+
+
+return (0);
+}
+
