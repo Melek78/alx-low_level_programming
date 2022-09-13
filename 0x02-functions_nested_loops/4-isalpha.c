fd2594aeff71c88253706d558a50fbca2d015f51
Melek78 <abemelek78@gmail.com>
2022-09-13 21:33:18 +0300

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,8 +5,8 @@
 * @c: the ASCII value of the character to be checked
 * Return: returns 0 if its a letter, 0 otherwise
 */
-{
 int _isalpha(int c)
+{
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
