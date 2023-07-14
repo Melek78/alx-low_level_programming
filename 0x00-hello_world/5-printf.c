7ccaa4eb0b672d0c68a2783f7b351db49c54704f
Melek78 <abemelek78@gmail.com>
2023-07-14 10:47:40 -0700

    5 final correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
 #define math 12
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
