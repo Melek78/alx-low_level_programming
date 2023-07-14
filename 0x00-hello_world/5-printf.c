bdc8079a28e758a505d71e2cc65fdaca0c16288e
Melek78 <abemelek78@gmail.com>
2023-07-14 10:41:10 -0700

    6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,9 @@
  *
  * Return: Always 0 (Success)
  */
+#define math 12
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,")
+	printf("with proper grammar, but the outcome is a piece of art,\n")
 		return (0);
 }
